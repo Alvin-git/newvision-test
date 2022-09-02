ffe858765f9ebb866e04d86e726a93f5caaf4354
AlvTarino <alvtarino@outlook.com>
2022-09-02 22:05:46 +0300

    Views

--- bookOrder_test/urls.py
+++ bookOrder_test/urls.py
@@ -1,4 +1,4 @@
-"""bookOrder_test URL Configuration
+"""crm1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls'))
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
